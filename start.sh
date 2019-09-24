7f8baa1ff65a2dedbcebb3b15365b718e5dd3c70
Carine I. SEMWAGA <semwagacarine@gmail.com>
2019-09-24 13:56:40 +0200

    Clone watchlist

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 export MOVIE_API_KEY='58aed921d71acba6bbb12787dbef553c'
 export SECRET_KEY='<Flask WTF Secret Key>'
-export MAIL_USERNAME='mjones6944@gmail.com'
-export MAIL_PASSWORD='2015s3red'
+export MAIL_USERNAME='semwagacarine@gmail.com'
+export MAIL_PASSWORD='gatete12'
 
 python3.6 manage.py server
 
