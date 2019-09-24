573b864e15ecfbf7b58fab4b7accba0349d04f04
Carine I. SEMWAGA <semwagacarine@gmail.com>
2019-09-24 20:47:28 +0200

    Create relationships

--- app/models.py
+++ app/models.py
@@ -10,20 +10,22 @@ class User(db.Model):
     username = db.Column(db.String(255), unique=True, nullable=False)
     email = db.Column(db.String(255), unique=True, nullable=False, index = True)
     password = db.Column(db.String(100), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+    comments = db.relationship('Comment', backref='author', lazy=True)
 
     def __repr__(self):
         return f'User {self.username}'
 
-class Pitch(db.Model):
+class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(255), nullable=False)
     content = db.Column(db.Text, nullable=False)
     category = db.Column(db.Text, nullable=False)
     posted_date = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
-    comments = db.relationship('comment', backref='post', lazy=True)
+    comments = db.relationship('Comment', backref='post', lazy=True)
 
     def __repr__(self):
-        return f"Pitch ( '{self.title}', '{self.category}', '{self.posted_date}')"
+        return f"Post ( '{self.title}', '{self.category}', '{self.posted_date}')"
     
 class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
